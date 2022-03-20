446761d24f899542bb1b5a79ed99c02dde665d4b
Pritam Khose <pritamkhose@gmail.com>
2022-03-20 17:28:33 +0530

    add jdk11

--- src/main/java/com/pritam/demo/swagger/SwaggerConfig.java
+++ src/main/java/com/pritam/demo/swagger/SwaggerConfig.java
@@ -1,27 +1,14 @@
 package com.pritam.demo.swagger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpEntity;
 
 import com.google.common.base.Predicates;
 
-import io.swagger.models.Contact;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -40,7 +27,8 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+//				.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build(); //.apiInfo(metadata());
 	}
 
